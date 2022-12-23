ba2be77574296f00346a6d03cb240b83de729ece
Sonal Shabir <103493764+sonal1502@users.noreply.github.com>
2022-12-23 17:50:53 +0530

    Create Find_Remainder.cpp

---
+++ Find_Remainder.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+          int a,b,c;
+          cin>>a>>b;
+               c=a%b;
+               cout<<"\n"<<c;
+     }
+     return 0;
+}
